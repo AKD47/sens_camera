5039ed4d82701351412c5d03ea62fc74ab18e977
AKD47 <van83horn@gmail.com>
2019-06-20 11:22:38 +0300

    mask

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -14,6 +14,31 @@
             </div>
             <div class="buy">
                 <h3 class="section-title">Приобрести</h3>
+                <div class="buy__box">
+                    <div class="buy__item">
+                        <span class="buy__icon buy__icon--first"></span>
+                        <h3 class="buy__descr">Покупка</h3>
+                    </div>
+                    <div class="buy__item">
+                        <span class="buy__icon buy__icon--second"></span>
+                        <h3 class="buy__descr">Рассрочка</h3>
+                    </div>
+                    <div class="buy__item">
+                        <span class="buy__icon buy__icon--third"></span>
+                        <h3 class="buy__descr"><span>Пакет</span> <br> “Все под контролем”</h3>
+                    </div>
+                    <div class="buy__item">
+                        <span class="buy__icon buy__icon--fourth"></span>
+                        <h3 class="buy__descr"><span>Пакет</span> <br> “Тысяча пятьсот”</h3>
+                    </div>
+                </div>
+            </div>
+            <div class="order">
+                <form action="" id="order-form" class="order__form">
+                    <p>Оставьте заявку, и мы позвоним вам в ближайшее время</p>
+                    <input id="form-phone" type="text" value="" min-length="10" max-length="10">
+
+                </form>
             </div>
         </div>
     </div>
@@ -24,7 +49,18 @@
 
 
     export default {
+        data() {
+            return {
+
+            }
+        },
         name: 'home',
-        components: {}
+        components: {},
+        methods: {
+            addPhone () {
+                let phone = document.getElementById('form-phone');
+
+            }
+        }
     }
 </script>
