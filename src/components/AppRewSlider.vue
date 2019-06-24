38bd551cd5d44018324bb49cce212262cf103de6
AKD47 <van83horn@gmail.com>
2019-06-24 11:19:49 +0300

    slider

--- src/components/AppRewSlider.vue
+++ src/components/AppRewSlider.vue
@@ -1,34 +1,71 @@
 <template lang="html">
     <div class="rew">
         <h3 class="section-title">Отзывы</h3>
-       <div class="rew__slider">
-           <div class="rew__item rew__item--active" data-index="0">
-               <div class="rew__avatar">
-                   <img src="../assets/images/ic-avatar.png" alt="ava">
-               </div>
-               <div class="rew__about">
-                   <h3>Алексей Гоно</h3>
-                   <p>Отличное и удобное приложение. <br> Классные возможности, отзывчивая поддержка.</p>
-               </div>
-           </div>
-           <div class="rew__item" data-index="2">
-               <div class="rew__avatar">
-                   <img src="../assets/images/ic-avatar.png" alt="ava">
-               </div>
-               <div class="rew__about">
-                   <h3>Алексей Гоно</h3>
-                   <p>Отличное и удобное приложение. <br> Классные возможности, отзывчивая поддержка.</p>
-               </div>
-           </div>
-           <button class="rew__trigger"></button>
-       </div>
+        <div id="rew-slider"  class="rew__slider">
+            <div v-for="slide in slides"  v-bind:class="['rew__item', (slide.isActive) ? 'rew__item--active': '']">
+                <div class="rew__avatar">
+                    <img src="../assets/images/ic-avatar.png" alt="ava">
+                </div>
+                <div class="rew__about">
+                    <h3>{{slide.title}}</h3>
+                    <p>{{slide.text}}</p>
+                </div>
+            </div>
+            <button id="rew-trigger" @click="nextSlide" class="rew__trigger"></button>
+        </div>
     </div>
 </template>
 
-<script>
+<script lang="js">
+
     export default {
-        name: "app-rew-slider"
+        name: "app-rew-slider",
+        data() {
+            return {
+                slides: [
+                    {
+                        title: 'Алексей Гоно',
+                        text: 'Отличное и удобное приложение. Классные возможности, отзывчивая поддержка.',
+                        isActive: true
+                    },
+                    {
+                        title: 'Гоно Алексей',
+                        text: 'Отличное и удобное приложение. Классные возможности, отзывчивая поддержка.',
+                        isActive: false
+                    },
+                    {
+                        title: 'Иван Юрьев',
+                        text: 'Отличное и удобное приложение. Классные возможности, отзывчивая поддержка.',
+                        isActive: false
+                    },
+                    {
+                        title: 'Алексей Васильев',
+                        text: 'Отличное и удобное приложение. Классные возможности, отзывчивая поддержка.',
+                        isActive: false
+                    }
+                ]
+            }
+
+        },
+        methods: {
+            nextSlide: function () {
+                for (let i = 0; i < this.slides.length; i++) {
+                    if (this.slides[i].isActive) {
+                        if (i === this.slides.length - 1) {
+                            this.slides[i].isActive = false;
+                            this.slides[0].isActive = true;
+                            break;
+                        } else {
+                            this.slides[i].isActive = false;
+                            this.slides[i + 1].isActive = true;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
     }
+
 </script>
 
 <style scoped>
